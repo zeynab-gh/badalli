ac38c290e09c553f4273d1976400c51b710037c0
zeynab <computer.1818@yahoo.com>
2024-07-19 21:19:50 +0430

    part

---
+++ badalli/shop/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path,include
+from .views import helloworld
+urlpatterns = [
+   
+    path('',helloworld)
+]
--- badalli/shop/views.py
+++ badalli/shop/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def helloworld(request):
+    return render(request,'index.html')
